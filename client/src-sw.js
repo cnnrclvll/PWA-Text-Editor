9188b95d2eacccad1709c472f5084be3e862113f
Connor Colvill <cnnrclvll@gmail.com>
2024-07-08 03:52:42 -0400

    service worker through inject manifest

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,30 +1,46 @@
+// --- SERVICE WORKER SOURCE --- //
+// ----------------------------- //
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+  // ^ service worker task recipes
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
+  // ^ catch and fetch handling
 const { registerRoute } = require('workbox-routing');
+  // ^ define routes and link to cache strategies
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
+  // ^ conditional caching
 const { ExpirationPlugin } = require('workbox-expiration');
+  // ^ expirations on cached responses
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+  // ^ precache and route files listed in manifest
 
 precacheAndRoute(self.__WB_MANIFEST);
+  // precache assets from workbox build through the manifest
+  // assets will be available offline
 
+// caching strategy
 const pageCache = new CacheFirst({
   cacheName: 'page-cache',
   plugins: [
     new CacheableResponsePlugin({
+      // only store responses with status codes of 0 or 200
       statuses: [0, 200],
     }),
     new ExpirationPlugin({
+      // 30 day expiration (d/h/m/s)
       maxAgeSeconds: 30 * 24 * 60 * 60,
     }),
   ],
 });
 
 warmStrategyCache({
+// prefetch and cache root URL and index.html
+// occurs when service worker is installed
   urls: ['/index.html', '/'],
   strategy: pageCache,
 });
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+// for navigation requests serve from cache first
 
 // TODO: Implement asset caching
 registerRoute();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,32 +1,67 @@
+// --- IMPORT --- //
+// -------------- //
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+  // create an HTML file for webpack bundle
 const WebpackPwaManifest = require('webpack-pwa-manifest');
+  // file references
 const path = require('path');
+  // node module for directory paths
 const { InjectManifest } = require('workbox-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+  // custom service worker
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
+
+// --- EXPORT --- //
+// webpack config //
+// --- object --- //
+// -------------- //
 
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
+      // application entry points
       main: './src/js/index.js',
       install: './src/js/install.js'
     },
     output: {
+      // output bundles use entry point names
+      // output directory name is 'dist'
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+    // webpack extensions
       new HtmlWebpackPlugin({
+      // create HTML file to be included in bundle
+        // uses specified template and title
         template: './index.html',
-        title: 'Webpack Plugin',
+        title: 'J.A.T.E',
       }),
-      new MiniCssExtractPlugin(),
       new InjectManifest({
-        swSrc: './src/sw.js',
-        swDest: 'service-worker.js',
+      // custom service worker
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new WebpackPwaManifest({
+      // creates PWA Manifest file
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E',
+        description: 'Takes notes with JavaScript syntax highlighting!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
       }),
     ],
 
