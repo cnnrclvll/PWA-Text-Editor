54414d4580c4e573520401bfe06d8039656bfb05
Connor Colvill <cnnrclvll@gmail.com>
2024-07-08 16:25:14 -0400

    getDb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// add to DB
 export const putDb = async (content) => {
   // ^ 'content' to be added to database
   console.log('Add to database.');
@@ -34,6 +34,25 @@ export const putDb = async (content) => {
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => {};
+export const getDb = async () => {
+  console.log('Get from database');
+  const database = await openDB('jate', 1);
+  // ^ open or create jate v1 database
+  const readOnly = database.transaction('jate', 'readonly');
+  // ^ read only transaction with jate object store
+  const store = readOnly.objectStore('jate');
+  // ^ create an object store from readonly transaction
+  const request = store.get(1);
+  // ^ retrieve data with id of 1
+  const result = await request;
+  // await completion of get
+  result
+    // ternary upon the condition of successful data retrieval
+    ? console.log('Data retrieved.', result.value)
+    : console.log('Failed to get data.');
+  return result?.value;
+  // return corresponding ternary option
+};
 
 initdb();
+// initiate (open or create) database
